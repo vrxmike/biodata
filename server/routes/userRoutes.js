74b76caee869f224f2a1fd45e829ea4d6b86eb82
vrxmike <alvinvrxmike@icloud.com>
2024-03-13 00:17:48 +0300

    update userRoutes.js

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * Controller for handling user-related operations.
+ * @type {Object}
+ */
+const userController = require('../controllers/userController');
+
+/**
+ * Route for handling the request to update the user's email.
+ * @name POST /request-email-update
+ * @function
+ * @memberof module:userRoutes
+ * @inner
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {void}
+ */
+router.post('/request-email-update', userController.requestEmailUpdate);
+
+/**
+ * Route for handling the verification of the email update request.
+ * @name GET /verify-email-update/:token
+ * @function
+ * @memberof module:userRoutes
+ * @inner
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {void}
+ */
+router.get('/verify-email-update/:token', userController.verifyEmailUpdate);
+
+module.exports = router;
