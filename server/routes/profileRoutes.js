7968100da3e0a08c598eae97e9c9f49eaf86770b
vrxmike <alvinvrxmike@icloud.com>
2024-06-10 04:55:22 +0300

    profileRoutes.js

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -20,18 +20,6 @@ const profileController = require('../controllers/profileController');
 router.post('/profiles', profileController.createProfile);
 
 /**
- * Route for getting a profile by ID.
- * @name GET /profiles/:id
- * @function
- * @memberof module:routes/profileRoutes
- * @inner
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- */
-router.get('/profiles/:id', profileController.getProfileById);
-
-/**
  * Route for updating a profile by ID.
  * @name PUT /profiles/:id
  * @function
@@ -42,17 +30,5 @@ router.get('/profiles/:id', profileController.getProfileById);
  * @param {Function} next - The next middleware function.
  */
 router.put('/profiles/:id', profileController.updateProfile);
-
-/**
- * Route for deleting a profile by ID.
- * @name DELETE /profiles/:id
- * @function
- * @memberof module:routes/profileRoutes
- * @inner
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- */
-router.delete('/profiles/:id', profileController.deleteProfile);
 
 module.exports = router;
